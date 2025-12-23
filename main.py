bd1e82cd1214cb134eb7e5cb944a1b7d886422f4
Prakhar Bhagat <prakharbhagat@Prakhars-MacBook-Air-2.local>
2025-12-23 22:25:19 +0530

    deploy

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ from sqlalchemy.orm import sessionmaker, declarative_base, Session
 # -------------------------------------------
 # DATABASE SETUP
 # -------------------------------------------
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://pb:mypassw@localhost/saltswap")
-if DATABASE_URL.startswith("postgres://"):
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    # Fallback to local if not on the server
+    DATABASE_URL = "postgresql://pb:mypassw@localhost/saltswap"
+
+# 2. Fix the URL prefix (Neon/Render use 'postgres://', SQLAlchemy needs 'postgresql://')
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+Engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
